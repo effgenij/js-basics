8eac3260901ff531017304f1c498451295463a44
Evgeniy <effgenij@gmail.com>
2024-02-10 08:36:30 +0300

    fix solution

--- 4-navigator/navigator.js
+++ 4-navigator/navigator.js
@@ -4,11 +4,11 @@
 // positionLat - текущее положение lat
 // positionLong - текущее положение long
 
-let addressLat;
-let addressLong;
-let positionLat;
-let positionLong;
+const addressLat = 20;
+const addressLong = 30;
+const positionLat = 5;
+const positionLong = 10;
 
-const distanceLength = Math.sqrt((positionLat - addressLat) ** 2 + (positionLong + addressLong) **  2); 
+const distanceLength = Math.sqrt((positionLat - addressLat) ** 2 + (positionLong - addressLong) **  2); 
 
 console.log(`Дистанция до объекта: ${distanceLength}`);
