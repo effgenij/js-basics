d29ec0d33eb3f001b1c9a44928c06efcdd1b9f7b
Evgeniy <effgenij@gmail.com>
2024-02-10 09:08:57 +0300

    add second solution

---
+++ 5-languages/languages.js
@@ -0,0 +1,21 @@
+const lang = '';
+
+switch (lang) {
+    case 'ru':
+        console.log('Добрый день!');
+        break;
+    case 'de':
+        console.log('Guten Tag!');
+        break;
+    case 'it':
+        console.log('Buon giorno!');
+        break;
+    case 'fr':
+        console.log('Bonjour!');
+        break;
+    case 'en':
+        console.log('Hello');
+        break;
+    default:
+        console.log('Неизвестный язык');
+}
